3485ae67893b2c68ea7e8d0b6d03b61c3259f360
Mikhail Shamanov <shamanovma@gmail.com>
2024-04-23 23:23:21 +0300

    Completed solution for task_1

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,43 @@
-import {encoded, translations} from './data.js'
+import {encoded, translations} from './data.js';
 
-console.log("Let's rock")
-console.log(encoded, translations)
+const ignoreFieldSet = new Set(['groupId', 'service', 'formatSize', 'ca']);
+const uniqueIds = new Set();
 
+function addUniqueId(id) {
+    if (!id) {
+        return;
+    }
 
+    uniqueIds.add(id);
+}
 
-// console.log(decoded)
+function translate(item) {
+    for (const key in item) {
+        if (!ignoreFieldSet.has(key)) {
+            const id = item[key];
+            const translation = translations[id];
+            if (translation === undefined) {
+                addUniqueId(id);
+            } else {
+                item[key] = translation;
+            }
+        }
+    }
+
+    return item;
+}
+
+function decode(data) {
+    if (!data || !Array.isArray(data)) {
+        return;
+    }
+
+    return data.map(translate);
+}
+
+const decoded = decode(encoded);
+
+// console.log('Let\'s rock');
+// console.log(encoded, translations);
+console.log(decoded);
+console.log(uniqueIds);
